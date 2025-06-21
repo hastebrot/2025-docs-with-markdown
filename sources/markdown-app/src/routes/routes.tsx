9d5b09db5fa0dc7e429f0bb22299deed7cbed466
Benjamin Gudehus <hastebrot@gmail.com>
2025-06-21 18:17:59 +0200

    [NOSTORY] feat: Markdown with remark and shiki. (#1)

--- sources/markdown-app/src/routes/routes.tsx
+++ sources/markdown-app/src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import { type RouteObject } from "react-router";
+import { IndexPage } from "./indexPage";
 
 export const routes: RouteObject[] = [
   // wrap.
-  { path: "/", Component: () => <div className="p-4">index</div> },
+  { path: "/", Component: () => <IndexPage /> },
 ];
