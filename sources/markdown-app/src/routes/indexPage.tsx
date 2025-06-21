8faa65755a8b259189404ffc93eee632e6fd480e
Benjamin Gudehus <hastebrot@gmail.com>
2025-06-21 19:59:15 +0200

    feat: Markdown columns and sections.

--- sources/markdown-app/src/routes/indexPage.tsx
+++ sources/markdown-app/src/routes/indexPage.tsx
@@ -1,22 +1,33 @@
 import rehypeShikiFromHighlighter from "@shikijs/rehype/core";
-import type { Root } from "hast";
+import type { Element, Root } from "hast";
 import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 import { createHighlighterCore } from "shiki/core";
 import { createOnigurumaEngine } from "shiki/engine/oniguruma";
 import { visit } from "unist-util-visit";
+import { classNames } from "../helpers/clsx";
+
+const yamlDocs = {
+  "01_intro.md": await import("../../assets/yaml-docs/01_intro.md?raw"),
+  "02_parse_stringify.md": await import("../../assets/yaml-docs/02_parse_stringify.md?raw"),
+  "03_options.md": await import("../../assets/yaml-docs/03_options.md?raw"),
+};
 
 export const IndexPage = () => {
+  const source = [
+    // wrap.
+    yamlDocs["01_intro.md"].default,
+    yamlDocs["02_parse_stringify.md"].default,
+    yamlDocs["03_options.md"].default,
+  ].join("\n");
+
   return (
     <main data-theme="dark" className="min-h-dvh">
-      <div className="prose-invert prose prose-base prose-zinc p-4">
-        <MarkdownSource source={yamlDocs["01_intro"].default} />
+      <div className="mx-auto w-fit">
+        <MarkdownSource source={source} />
       </div>
     </main>
   );
-};
-
-const yamlDocs = {
-  "01_intro": await import("../../assets/yaml-docs/01_intro.md?raw"),
 };
 
 type MarkdownSourceProps = {
@@ -24,6 +35,78 @@ type MarkdownSourceProps = {
 };
 
 const MarkdownSource = (props: MarkdownSourceProps) => {
+  const rehypeSlatedocs = function rehypeSlatedocs() {
+    return function transform(tree: Root) {
+      const createColumnLeft = () =>
+        ({
+          type: "element",
+          tagName: "div",
+          properties: {
+            className: classNames(
+              "prose max-w-[70ch] prose-invert prose-base prose-zinc",
+              "p-8 w-full border-x border-t border-(--border-base)"
+            ),
+          },
+          children: [],
+        } as Element);
+      const createColumnRight = () =>
+        ({
+          type: "element",
+          tagName: "div",
+          properties: {
+            className: classNames(
+              "prose max-w-[60ch] prose-invert prose-base prose-zinc",
+              "p-8 w-full border-r border-(--border-base)"
+            ),
+          },
+          children: [],
+        } as Element);
+      const createBlock = () =>
+        ({
+          type: "element",
+          tagName: "div",
+          properties: {
+            className: classNames("grid grid-cols-[auto_auto] mt-[-1px]"),
+          },
+          children: [],
+        } as Element);
+
+      const block = createBlock();
+      let columnLeft = createColumnLeft();
+      let columnRight = createColumnRight();
+      block.children.push(columnLeft);
+      block.children.push(columnRight);
+      while (true) {
+        const child = tree.children.shift();
+        if (child === undefined) {
+          break;
+        }
+        if (child.type === "element") {
+          if (child.tagName === "h1") {
+            columnLeft = createColumnLeft();
+            columnRight = createColumnRight();
+            block.children.push(columnLeft);
+            block.children.push(columnRight);
+          } else if (child.tagName === "h2") {
+            columnLeft = createColumnLeft();
+            columnRight = createColumnRight();
+            block.children.push(columnLeft);
+            block.children.push(columnRight);
+          }
+          if (child.tagName === "blockquote") {
+            columnRight.children.push(child);
+          } else if (child.tagName === "pre") {
+            columnRight.children.push(child);
+          } else {
+            columnLeft.children.push(child);
+          }
+        }
+      }
+
+      tree.children.unshift(block);
+    };
+  };
+
   const slugify = (text: string) => text.toLowerCase().replace(/\s+/g, "-");
   const rehypeSlug = function rehypeSlug() {
     return function transform(tree: Root) {
@@ -45,8 +128,13 @@ const MarkdownSource = (props: MarkdownSourceProps) => {
           return <h1 className="font-[700]" {...props}></h1>;
         },
       }}
-      remarkPlugins={[]}
+      remarkPlugins={[
+        // wrap.
+        remarkGfm,
+      ]}
       rehypePlugins={[
+        // wrap.
+        rehypeSlatedocs,
         rehypeSlug,
         [
           rehypeShikiFromHighlighter,
